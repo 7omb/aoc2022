850bb3e1776853c7194fdd44938429fed351b9f0
Tom Bärwinkel <dev@baerwinkel.org>
2022-12-04 09:38:00 +0100

    Day 4 pt 1

---
+++ day04/Main.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE LambdaCase #-}
+
+import Control.Arrow ((>>>))
+import Data.Bifunctor (bimap, second)
+
+main :: IO ()
+main = interact (lines >>> contains)
+
+contains :: [String] -> String
+contains = map (split ',' >>> bimap toInterval toInterval >>> uncurry fullyContains) >>> filter (== True) >>> length >>> show
+
+split :: Char -> String -> (String, String)
+split c = span (/= c) >>> second (drop 1)
+
+data Interval = Interval Int Int
+  deriving (Show)
+
+toInterval :: String -> Interval
+toInterval = split '-' >>> bimap read read >>> uncurry Interval
+
+fullyContains :: Interval -> Interval -> Bool
+fullyContains (Interval l1 u1) (Interval l2 u2) = (l2 >= l1 && u2 <= u1) || (l1 >= l2 && u1 <= u2)
