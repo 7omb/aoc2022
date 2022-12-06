cd294b66609aab648ff61d17a9c5bfc4d5c5ac41
Tom Bärwinkel <dev@baerwinkel.org>
2022-12-06 07:42:20 +0100

    Day 6 pt 2

--- day06/Main.hs
+++ day06/Main.hs
@@ -5,16 +5,19 @@ import Data.Set qualified as S
 main :: IO ()
 main = interact (lines >>> head >>> marker)
 
+headerLength :: Int
+headerLength = 14
+
 marker :: String -> String
 marker input = show $ foldl' search window remaining
   where
-    window = (4, take 4 input)
-    remaining = drop 4 input
+    window = (headerLength, take headerLength input)
+    remaining = drop headerLength input
 
 type Window = (Int, String)
 
 search :: Window -> Char -> Window
 search (index, sequence) next =
-  if length (S.fromList sequence) == 4
+  if length (S.fromList sequence) == headerLength
     then (index, sequence)
     else (index + 1, drop 1 sequence ++ [next])
