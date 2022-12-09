5d95bbdd65c61204680d1570d97dc29c45748e97
Tom Bärwinkel <dev@baerwinkel.org>
2022-12-09 19:35:20 +0100

    Day 9 pt 2

--- day09/Main.hs
+++ day09/Main.hs
@@ -4,46 +4,49 @@ import Control.Arrow ((>>>))
 import Data.List (foldl')
 import Data.Set qualified as S
 
-main :: IO ()
-main = interact (lines >>> parseMoves >>> run >>> visited >>> S.size >>> show)
+numKnots :: Int
+numKnots = 10
 
-data Move = L | R | U | D
+main :: IO ()
+main = interact (lines >>> parseMoves >>> run numKnots >>> snd >>> S.size >>> show)
+
+data Direction = L | R | U | D
   deriving (Show, Read)
 
-parseMoves :: [String] -> [Move]
+parseMoves :: [String] -> [Direction]
 parseMoves = map (words >>> parseLine) >>> concat
   where
     parseLine [direction, number] =
       let amount = read number
-          move = read direction :: Move
+          move = read direction :: Direction
        in replicate amount move
 
-type Tail = (Int, Int)
+type Position = (Int, Int)
 
-type Head = (Int, Int)
+type Visited = S.Set Position
 
-type Visited = S.Set Tail
+type State = ([Position], Visited)
 
-type State = (Head, Tail, Visited)
+run :: Int -> [Direction] -> State
+run numKnots = foldl' move (initialPositions, S.empty)
+  where
+    initialPositions = replicate numKnots (0, 0)
 
-run :: [Move] -> State
-run = foldl' move ((0, 0), (0, 0), S.empty)
+move :: State -> Direction -> State
+move (currentHead : tails, visited) direction =
+  let newHead = moveHead currentHead direction
+      newPositions = foldl' (\(last : rest) tail -> moveTail tail last : last : rest) [newHead] tails
+      newVisited = S.insert (head newPositions) visited
+   in (reverse newPositions, newVisited)
 
-move :: State -> Move -> State
-move (head, tail, visited) move =
-  let newHead = moveHead head move
-      newTail = moveTail tail newHead
-      newVisited = S.insert newTail visited
-   in (newHead, newTail, newVisited)
-
-moveHead :: Head -> Move -> Head
+moveHead :: Position -> Direction -> Position
 moveHead (x, y) = \case
   L -> (x - 1, y)
   R -> (x + 1, y)
   U -> (x, y + 1)
   D -> (x, y - 1)
 
-moveTail :: Tail -> Head -> Tail
+moveTail :: Position -> Position -> Position
 moveTail (tx, ty) (hx, hy)
   | abs dx <= 1 && abs dy <= 1 = (tx, ty)
   | dx == 0 = if dy > 0 then (tx, ty + 1) else (tx, ty - 1)
@@ -55,6 +58,3 @@ moveTail (tx, ty) (hx, hy)
   where
     dx = hx - tx
     dy = hy - ty
-
-visited :: State -> S.Set Tail
-visited (_, _, v) = v
