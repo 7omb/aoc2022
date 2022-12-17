8201575f4f2aa8627d510c60720a21ca653cbf52
Tom Bärwinkel <dev@baerwinkel.org>
2022-12-17 15:41:38 +0100

    Day 12 pt 2

--- day12/Main.hs
+++ day12/Main.hs
@@ -3,12 +3,12 @@ import Data.Array qualified as A
 import Data.Char (ord)
 import Data.List (find)
 import Data.Map.Strict qualified as M
-import Data.Maybe (fromJust)
+import Data.Maybe (fromJust, isJust)
 import Data.Sequence qualified as Seq
 import Data.Set qualified as Set
 
 main :: IO ()
-main = interact (lines >>> parse >>> transform >>> run >>> shortestDistance >>> show)
+main = interact (lines >>> parse >>> transform >>> run >>> shorestDistances >>> minimumDistance >>> show)
 
 type Grid = A.Array Position Char
 
@@ -24,14 +24,16 @@ parse input =
     numCols = length . head $ input
     withIndices = zip [0 ..] $ map (zip [0 ..]) input
 
-transform :: Grid -> (Position, Position, Grid)
+transform :: Grid -> (Position, [Position], Grid)
 transform input =
-  (start, end, array)
+  (start, ends, array)
   where
     findChar c = fst . fromJust . find (\e -> snd e == c) . A.assocs
-    start = findChar 'S' input
-    end = findChar 'E' input
-    array = input A.// [(start, 'a'), (end, 'z')]
+    start = findChar 'E' input
+    end = findChar 'S' input
+    array = input A.// [(start, 'z'), (end, 'a')]
+
+    ends = map fst $ filter (\(i, e) -> e == 'a') $ A.assocs array
 
 data Graph = Graph
   { parents :: M.Map Position Position,
@@ -39,9 +41,9 @@ data Graph = Graph
   }
   deriving (Show)
 
-run :: (Position, Position, Grid) -> (Position, Graph)
-run (start, end, grid) =
-  (end, go grid inProgress visited parents distances)
+run :: (Position, [Position], Grid) -> ([Position], Graph)
+run (start, ends, grid) =
+  (ends, go grid inProgress visited parents distances)
   where
     inProgress = Seq.singleton start
     visited = Set.singleton start
@@ -75,10 +77,13 @@ inGrid grid (row, col) =
 
 isAllowed :: Grid -> Position -> Position -> Bool
 isAllowed grid current next =
-  let currentHeight = grid A.! current
-      nextHeight = grid A.! next
-   in ord nextHeight <= ord currentHeight + 1
+  let currentHeight = ord 'z' - ord (grid A.! current)
+      nextHeight = ord 'z' - ord (grid A.! next)
+   in nextHeight <= currentHeight + 1
 
-shortestDistance :: (Position, Graph) -> Int
-shortestDistance (end, graph) =
-  distances graph M.! end
+shorestDistances :: ([Position], Graph) -> [Maybe Int]
+shorestDistances (ends, graph) =
+  map (\e -> distances graph M.!? e) ends
+
+minimumDistance :: [Maybe Int] -> Int
+minimumDistance = filter isJust >>> map fromJust >>> minimum
