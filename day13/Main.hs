a6434bc18653f0825002de29467abb4c47a43ffd
Tom Bärwinkel <dev@baerwinkel.org>
2022-12-25 09:50:37 +0100

    Day 13 pt 2

--- day13/Main.hs
+++ day13/Main.hs
@@ -3,12 +3,13 @@
 import Control.Applicative ((<|>))
 import Control.Arrow ((>>>))
 import Data.Either (fromRight)
+import Data.List (sort)
 import Data.Void (Void)
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
 main :: IO ()
-main = interact (parser >>> map (uncurry compare) >>> sumIndices >>> show)
+main = interact (parser >>> flatten >>> addDividers >>> sort >>> signal >>> show)
 
 parser :: String -> [(Packet, Packet)]
 parser = parse fileParser "" >>> fromRight []
@@ -39,3 +40,12 @@ instance Ord Cell where
 
 sumIndices :: [Ordering] -> Int
 sumIndices = zip [1 ..] >>> filter (snd >>> (== LT)) >>> map fst >>> sum
+
+flatten :: [(Packet, Packet)] -> [Packet]
+flatten = foldr (\(p1, p2) acc -> p1 : p2 : acc) []
+
+addDividers :: [Packet] -> [Packet]
+addDividers ps = [Nested [Num 2]] : [Nested [Num 6]] : ps
+
+signal :: [Packet] -> Int
+signal = zip [1 ..] >>> filter (snd >>> (\p -> p == [Nested [Num 2]] || p == [Nested [Num 6]])) >>> map fst >>> product
